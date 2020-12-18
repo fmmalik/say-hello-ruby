82c49d291526e72c4306602cb83a2210df53a1fc
Farah Malik <my.github.farahmalik82@hotmail.com>
2020-12-18 12:38:08 -0500

    commit tree 6c6d85407f31c209e819c8d05b5a74b05b5d0b83

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,19 @@
-# Build your say_hello method here
+def greeting(name = "Ruby Programmer")
+    puts "Hello, Gabriela!"
+end
+greeting(name = "Ruby Programmer")
+
+def greeting(name, language="Ruby")
+    puts "Hello #{language} Programmer."
+end
+greeting(name, language="Ruby")
+
+def say_hello(name)
+    puts "Hello #{name}!"
+end
+say_hello(name)
+
+def say_hello(name = "Ruby Programmer")
+    puts "Hello #{name}!"
+end
+say_hello(name = "Ruby Programmer")
